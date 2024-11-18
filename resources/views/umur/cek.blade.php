34297d36e488f2da0b4e2b9ecd3fb559f699363c
idn-fahmifn <fahmifn.idn@gmail.com>
2024-11-18 14:33:48 +0700

    cekumur

---
+++ resources/views/umur/cek.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cek Umur</title>
+</head>
+<body>
+
+    <h1>Halaman Cek Umur</h1>
+
+    <form action="">
+        <div class="form-control">
+            <label>Berapa Umur Kamu?</label>
+            <input type="number" name="umur">
+        </div>
+        <div class="form-control">
+            <button type="submit">Masuk</button>
+        </div>
+    </form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,6 @@ Route::get('profile/{param}', function($param){
 
 // prefix => memberikan nama grup nya.
 Route::prefix('training')->group(function(){
-
     // ini untuk menampilkan localhost:8000/training/mtcna
     Route::get('mtcna', function(){
         return 'ini adalah halaman MTCNA';
@@ -40,6 +39,25 @@ Route::prefix('training')->group(function(){
         return 'ini adalah halaman CCNA';
         // memberikan name untuk uri CCNA
     })->name('training-ccna');
-
 });
+
+// fallback untuk mengarahkan etika user memanggil halaman yang routenya tidak didefinisikan.
+Route::fallback(function(){
+    return '404, Halamannya gaada';
+});
+
+// CRUD 
+/**
+ * 1. Routing untuk halaman index (get::index) -> menampilkan semua data,
+ * 2. Routing untuk halaman create (get::create) -> menampilkan halaman,
+ * 3. Routing untuk mengirimkan data (post::kirim-data) -> untuk mengirimkan data ke database.
+ * 4. Routing untuk memanggil data spesifik (get::detail) -> untuk melihat data secara spesifik.
+ * 5. Routing untuk edit data (get::edit) -> untuk menampilkan halaman edit.
+ * 6. Routing mengirim data yang sudah diedit (post::edit) -> untuk mengirimkan data yang sudah diedit.
+ * 7. Routing mengirim menghapus data (post::edit) -> untuk menghapus data secara spesifik.
+ */
+
+// Route::resource('profile', ProfileController::class);
+
+
 
