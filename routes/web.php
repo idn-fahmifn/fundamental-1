f77483bad5df0e242c77be03685778da2e797109
idn-fahmifn <fahmifn.idn@gmail.com>
2024-11-18 13:30:04 +0700

    prefix

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Simple Routing.
+Route::get('profile', function(){
+    return view('profile');
+});
+
+// Routing dengan parameters
+Route::get('profile/{param}', function($param){
+    return 'Ini adalah halaman profile dari :' .$param ;
+});
+
+
+// Route grouping dengan prefix
+Route::prefix('training')->group(function(){
+
+    Route::get('mtcna', function(){
+        return 'ini adalah halaman MTCNA';
+    });
+
+    Route::get('laravel', function(){
+        return 'ini adalah halaman Laravel';
+    });
+
+    Route::get('ccna', function(){
+        return 'ini adalah halaman CCNA';
+    });
+
+});
+
